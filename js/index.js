1456bd2217865693903445221cd133256af383d3
A. Calleja <albacalpa@gmail.com>
2022-07-19 19:42:58 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
 //
+
+let hacker1 = "Tom";
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Jerry";
+
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
